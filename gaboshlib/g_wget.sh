1d7444984a8f4fc4d84a976b795c5df0a1de50f2
olli <>
2025-06-03 15:34:52 +0200

    use proxy if given via g_proxys

--- gaboshlib/g_wget.sh
+++ gaboshlib/g_wget.sh
@@ -1,7 +1,18 @@
 function g_wget {
-  wget -T 10 -t 2 \
-   --header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" \
-   --header="Content-Type: application/json" \
-   --header="Accept-Language: en-US,en," \
-   $@
+
+  [[ -z "$g_proxys" ]] && g_proxys="none"
+  
+  for g_proxy in $g_proxys
+  do
+    export http_proxy=$g_proxy
+    export https_proxy=$g_proxy
+    export ftp_proxy=$g_proxy
+    [[ $g_proxy = none ]] && unset http_proxy https_proxy ftp_proxy
+
+    wget -T 10 -t 2 \
+     --header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" \
+     --header="Content-Type: application/json" \
+     --header="Accept-Language: en-US,en," \
+     $@
+  done
 }
