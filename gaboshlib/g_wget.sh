fb8d896ae43da3c7186aff7f124b40ebd3ff801d
olli <>
2025-06-03 16:06:14 +0200

    break if RC is 0

--- gaboshlib/g_wget.sh
+++ gaboshlib/g_wget.sh
@@ -13,6 +13,6 @@ function g_wget {
      --header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" \
      --header="Content-Type: application/json" \
      --header="Accept-Language: en-US,en," \
-     $@
+     $@ && break
   done
 }
