24c5d080511ba3793dd99dd69c7adfb32e35f860
olli <>
2025-09-12 19:56:52 +0200

    fix: check for uppcer case E too

--- gaboshlib/g_num_exponential2normal_file.sh
+++ gaboshlib/g_num_exponential2normal_file.sh
@@ -2,7 +2,7 @@ function g_num_exponential2normal_file {
   # changes expionential numbers in normal notation in given file
   local g_file=$1
   local g_substitution g_exnum
-  for g_exnum in $(egrep -o -i "[0-9]+\.[0-9]+e[\+\-][0-9]+" "$g_file" | sort -u)
+  for g_exnum in $(egrep -o -i "[0-9]+\.[0-9]+[eE][\+\-][0-9]+" "$g_file" | sort -u)
   do
     g_num_exponential2normal $g_exnum && g_substitution="${g_substitution}s/${g_exnum}/${g_num_exponential2normal_result}/g;"
   done
