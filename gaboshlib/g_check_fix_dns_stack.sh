64255179354d4f4d68c0b5ebdb8b6fc5e2d51009
olli <>
2025-11-19 17:56:25 +0100

    better tor support and DoHoT

--- gaboshlib/g_check_fix_dns_stack.sh
+++ gaboshlib/g_check_fix_dns_stack.sh
@@ -7,15 +7,36 @@ function g_check_fix_dns_stack {
 
 function g_check_dns_stack  {
   local testhost
+
+  # if we use DoHoT first check tor
+  if [[ -s /etc/systemd/resolved.conf.d/DoHoT.conf ]]
+  then
+    g_check_tor || g:_echo_warn "DoHoT: Tor seems not to work"
+  fi
+
   resolvectl flush-caches >/dev/null 2>&1
   nscd -i hosts >/dev/null 2>&1
   for testhost in $g_testhosts
   do
+
+    # if we use DoHoT check first dnscrypt-proxy
+    if [[ -s /etc/systemd/resolved.conf.d/DoHoT.conf ]]
+    then
+      if ! host -W1 -p 5354 $testhost 127.0.0.55 >>"${g_tmp}/g_check_dns_stack_output" 2>&1
+      then
+        g_echo_warn "Internet DNS over dnccrypt-proxy failed (testhost: $testhost)"
+        systemctl restart dnscrypt-proxy.service
+        sleep 5
+      fi
+    fi
+    
+    # general DNS Check
     if host -W1 $testhost >>"${g_tmp}/g_check_dns_stack_output" 2>&1
     then
       g_echo "Internet DNS connection OK (testhost: $testhost)"
       return 0
     fi
+
   done
   g_echo_warn "DNS Resoulution seems broken testshosts: $g_testhosts - $(cat "${g_tmp}/g_check_dns_stack_output")"
   return 1
---
+++ gaboshlib/g_check_tor.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function g_check_tor {
+  local torrestart=false
+
+  # check socks5
+  if ! curl -s --connect-timeout 3 --socks5-hostname $g_tor_host:$g_tor_socks5_port https://check.torproject.org/api/ip | jq -a .IsTor | grep -q '^true$'
+  then
+    g_echo_error "Tor over socks5 not working"
+    torrestart=true
+  fi
+
+  # Check proxy
+  if ! curl -s --connect-timeout 3 --proxy $g_tor_host:$g_tor_proxy_port https://check.torproject.org/api/ip | jq -a .IsTor | grep -q '^true$'
+  then
+    g_echo_error "Tor proxy not working"
+    torrestart=true
+  fi
+
+  # check for restart
+  if [[ $torrestart = "true" ]]
+  then
+    g_echo_warn "Restarting Tor"
+    systemctl restart tor.service
+    return 1
+  fi
+  
+  return 0
+}
+
