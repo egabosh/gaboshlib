05af8f278bd50d7002f96a715393bb7ef2effd2d
olli <>
2025-12-01 11:30:26 +0100

    better debugging

--- gaboshlib/g_check_tor.sh
+++ gaboshlib/g_check_tor.sh
@@ -3,18 +3,23 @@
 function g_check_tor {
   local torrestart=false
 
+  curl -s --connect-timeout 7 --socks5-hostname $g_tor_host:$g_tor_socks5_port https://check.torproject.org/api/ip >${g_tmp}/check.torproject.org-socks.json
+  curl -s --connect-timeout 7 --proxy $g_tor_host:$g_tor_proxy_port https://check.torproject.org/api/ip >${g_tmp}/check.torproject.org-proxy.json
+
   # check socks5
-  if ! curl -s --connect-timeout 7 --socks5-hostname $g_tor_host:$g_tor_socks5_port https://check.torproject.org/api/ip | jq -a .IsTor | grep -q '^true$'
+  if ! cat $g_tmp/check.torproject.org-socks.json | jq -a .IsTor | grep -q '^true$'
   then
-    g_echo_error "Tor over socks5 not working"
+    g_echo_error "Tor over socks5 not working
+$(cat ${g_tmp}/check.torproject.org-socks.json)"
     torrestart=true
   fi
 
   # Check proxy
-  if ! curl -s --connect-timeout 7 --proxy $g_tor_host:$g_tor_proxy_port https://check.torproject.org/api/ip | jq -a .IsTor | grep -q '^true$'
+  if ! ${g_tmp}/check.torproject.org-proxy.json | jq -a .IsTor | grep -q '^true$'
   then
-    g_echo_error "Tor proxy not working"
-    torrestart=true
+    g_echo_error "Tor proxy not working
+$(cat ${g_tmp}/check.torproject.org-proxy.json)"
+    #torrestart=true
   fi
 
   # check for restart
