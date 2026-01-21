13dcdfcf45c8710a3c435b29dfa00371a02365be
olli <>
2026-01-21 10:11:59 +0100

    retry on all errors ans stop tor if not locally used

--- gaboshlib/g_check_tor.sh
+++ gaboshlib/g_check_tor.sh
@@ -6,8 +6,10 @@ function g_check_tor {
 
   for curl_opts in "--socks5-hostname $g_tor_host:$g_tor_socks5_port" "--proxy $g_tor_host:$g_tor_proxy_port"
   do
+    try=1
+    
     # check socks5
-    curl="curl --retry 3 --retry-delay 5 --retry-connrefused --connect-timeout 5 $curl_opts https://check.torproject.org/api/ip"
+    curl="curl --retry 3 --retry-delay 5 --retry-all-errors --connect-timeout 5 $curl_opts https://check.torproject.org/api/ip"
     $curl >${g_tmp}/check.torproject.org.json 2>${g_tmp}/curl.err
     rc=$?
     if ! cat $g_tmp/check.torproject.org.json | jq -a .IsTor | grep -q '^true$'
@@ -19,6 +21,8 @@ $(cat ${g_tmp}/check.torproject.org.json)"
       torrestart=true
     else
       g_echo "Tor working ($curl_opts): $(cat ${g_tmp}/check.torproject.org.json)"
+      # stop local tor if we do not use it on localhost
+      [[ $g_tor_host != "127.0.0.1" ]] && systemctl stop tor.service
     fi
     rm ${g_tmp}/check.torproject.org.json
   done
